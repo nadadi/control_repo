cecb196e6ee19bc9a02c894b136a018ab5ec049c
Nathal Dawson Díaz <nathaldawsondiaz@gmail.com>
2022-10-20 17:33:21 -0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file{'/opt/minecraft':
     ensure => directory,
   }
-  file {'opt/minecraft/server.jar':
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
   }
